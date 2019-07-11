0eddd04f289f22b6b53b1e37a5bd2cb803678c4e
teej387 <teej387@yahoo.com>
2019-07-10 19:01:44 -0500

    Created action creators and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,9 +12,22 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      const {total} = action.payload
+      const addTotal = parseInt(total) + 1
+
+      return {
+        ...state,
+        [total]: addTotal,
+      }
+
     case DECREMENT:
-    // Fill in the body of this case
+      const subTotal = parseInt(total) - 1
+
+      return {
+        ...state,
+        [total]: subTotal,
+      }
+
     default:
       return state;
   }
